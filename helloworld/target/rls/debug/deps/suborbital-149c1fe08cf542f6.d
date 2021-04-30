b0d323e2a0a2eac5927257e02814bef5d00becda
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-04-29 22:11:06 -0600

    Initial commit

---
+++ helloworld/target/rls/debug/deps/suborbital-149c1fe08cf542f6.d
@@ -0,0 +1,5 @@
+/home/caleb/suborbital/playground/comment-server/helloworld/target/rls/debug/deps/suborbital-149c1fe08cf542f6.rmeta: /home/caleb/.cargo/registry/src/github.com-1ecc6299db9ec823/suborbital-0.9.2/src/lib.rs
+
+/home/caleb/suborbital/playground/comment-server/helloworld/target/rls/debug/deps/suborbital-149c1fe08cf542f6.d: /home/caleb/.cargo/registry/src/github.com-1ecc6299db9ec823/suborbital-0.9.2/src/lib.rs
+
+/home/caleb/.cargo/registry/src/github.com-1ecc6299db9ec823/suborbital-0.9.2/src/lib.rs:
---
+++ helloworld/target/wasm32-wasi/release/deps/helloworld.d
@@ -0,0 +1,7 @@
+/root/runnable/target/wasm32-wasi/release/deps/helloworld.wasm: src/lib.rs
+
+/root/runnable/target/wasm32-wasi/release/deps/libhelloworld.rlib: src/lib.rs
+
+/root/runnable/target/wasm32-wasi/release/deps/helloworld.d: src/lib.rs
+
+src/lib.rs:
